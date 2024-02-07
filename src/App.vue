d2c432548d38c9bff0f21c90be741b119077f2b4
Kanakubo Masamichi <kanakubomasamichi@Kanakubononotobukkukonpyuta.local>
2024-02-07 21:12:09 +0900

    feat: main layout section done and menuItem component made

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-
+import MenuItem from './components/MenuItem.vue'
 import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
 import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
 import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
@@ -34,9 +34,10 @@ let openMenu = ref(false)
             <ChevronRight fillColor="#FFFFFF" :size="30" />
         </button>
       </div>
-
-      <!-- 動画7:49まで再生 -->
-      <button class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer">
+      <button 
+        @click="openMenu = !openMenu" :class="openMenu ? 'bg-[#282828] ' : 'bg-black'"
+        class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
+      >
         <div class="flex items-center">
           <img
             class="rounded-full"
@@ -48,7 +49,66 @@ let openMenu = ref(false)
           <ChevronUp v-else @click="openMenu = false" fillColor="#FFFFFF" :size="25" />
         </div>
       </button>
+
+      <span
+        v-if="openMenu"
+        class="fixed w-[190px] bg-[#282828] z-50 shadow-2xl rounded-sm top-[52px] right-[35px] p-1 cursor-pointer"
+      >
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">Profile</li>
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]">Log out</li>
+        </ul>
+
+      </span>
     </div>
+     <div
+       id="sideNav"
+       class="fixed w-[240px] p-6 bg-black z-50 h-[100%]"
+     >
+      <RouterLink to="/">
+        <img width="125" src="/images/icons/spotify-logo.png"  />
+      </RouterLink>
+      <div class="my-8">
+        <ul>
+          <RouterLink to="/">
+            <MenuItem class="ml-[1px]" :iconSize="23" name="Home" iconString="home" pageUrl="/"/>
+          </RouterLink>
+          <RouterLink to="/search">
+            <MenuItem class="ml-[1px]" :iconSize="23" name="Search" iconString="search" pageUrl="/search"/>
+          </RouterLink>
+          <RouterLink to="/library">
+            <MenuItem class="ml-[1px]" :iconSize="23" name="Library" iconString="library" pageUrl="/library"/>
+          </RouterLink>
+          <div class="py-3.5">
+            <MenuItem :iconSize="24" name="Create Playlist" iconString="playlist" pageUrl="/playlist"/>
+            <MenuItem :icon-size="27" name="Liked Songs" iconString="liked" pageUrl="/liked"/>
+          </div>
+        </ul>
+        <div class="border-b border-b-gray-700"></div>
+      </div>
+      <ul>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #1</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #2</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #3</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #4</li>
+      </ul>
+     </div>
+  </div>
+  <div
+    class="
+      w-[calc(100%-240px)]
+      fixed
+      right-0
+      top-0
+      h-full
+      bg-gradient-to-b
+      from-[#1C1C1C]
+      to-black
+    "
+  >
+    <div class="mt-[70px]"></div>
+      <RouterView />
+    <div class="mb-[100px]"></div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import router from './router'
 import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
@@ -13,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(pinia)
+app.use(router)
 
 app.mount('#app')
